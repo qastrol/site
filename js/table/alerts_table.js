6c6644fc80f84bf363c7b6c99e8da870f13c2413
qastrol <187283980+qastrol@users.noreply.github.com>
2025-12-01 21:45:57 +0100

    Update alerts_table.js

--- js/table/alerts_table.js
+++ js/table/alerts_table.js
@@ -313,5 +313,5 @@
 	{ name: "Tito Dick \"Dickman\"", description: "Tito Dick \"Dickman\", een karakter van één van de slechtst beoordeelde tekenfilms allertijden: The Nutshack", code: "<tito>", category: ["TV"], type: "Video-alert", year: 2025},
 	{ name: "De Mus", description: "Tsjielp tsjielp tsjielp", code: "<mus>", category: ["Dieren"], type: "Video-alert", year: 2025},
 	{ name: "3D Pinball for Windows - Space Cadet", description: "Speel het iconische pinballspel van Windows XP: Space Cadet. Je kan niet de flippers bewegen, maar wel zieke scores behalen. Je hebt 30 seconden om een kaulo harde score te zetten", code: "<pinball>", category: ["Gaming"], type: "Interactieve alert", year: 2025},
-	{ name: "Alert voor bioscoop", description: "Beschrijving voor bioscoop", code: "<bioscoop>" },
-	{ name: "Alert voor hartslag", description: "Beschrijving voor hartslag", code: "<hartslag>" }];
+	{ name: "Qastrol's hartslag", description: "Zie de actuele hartslag van Qastrol", code: "<hartslag>", category: ["Qastrol"], type: "Interactieve alert", year: 2025},
+	{ name: "Pikachu's Jukebox", description: "Eén van de outro-nummers van het tweede deel van de Kanto anime en de Orange Island anime speelt af", code: "<pikachu>", category: ["TV"], type: "Video-alert", year: 2025},];
